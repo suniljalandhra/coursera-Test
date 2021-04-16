cecf603aaaae501c0aeed73b539d43e8d58a1657
Sunil kumar Jalandhra <jalandhrasunil99@gmail.com>
2021-04-17 00:42:11 +0530

    DOM

---
+++ DOM/script.js
@@ -0,0 +1,8 @@
+function sayHello() {
+    var name = 
+        document.getElementById("name").value
+        var message = "<h2>hello" + name + "!</h2>"
+    
+    document.getElementById("content")
+        .innerHTML = message;
+}
